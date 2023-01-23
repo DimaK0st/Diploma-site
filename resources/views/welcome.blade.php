4259112e9f8ca7cb4f71aa7e9b83251496bae437
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2023-01-23 23:30:56 +0200

    Add Meta And htaccess

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>EducationalSite</title>
+    <meta name="description"
+          content="Безкоштовний сайт за допомогою якого можна виконувати дистанційне навчання у будь-якій точці світу та для будь-якої людини.">
+    <meta name="keywords" content="онлайн навчання, онлайн навчання безкоштовно,
+        курси онлайн навчання, школа онлайн навчання, навчання нуля онлайн безкоштовно, навчання мов онлайн,
+        англійська онлайн навчання, дистанційне навчання онлайн, дистанційне навчання, дистанційне, навчання,
+        школа дистанційного навчання, дистанційне навчання школи, школи дистанційного навчання, школа, університет, технікум"/>
+    <meta name="language" content="uk">
 
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="{{asset('js/app.css')}}" rel="stylesheet"/>
-        <link href="{{asset('js/reset.css')}}" rel="stylesheet"/>
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="{{asset('js/app.css')}}" rel="stylesheet"/>
+    <link href="{{asset('js/reset.css')}}" rel="stylesheet"/>
 
-        @viteReactRefresh
-        @vite('resources/js/app.jsx')
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
 
-    </head>
-    <body class="antialiased">
-        <div id="root"></div>
-    </body>
+</head>
+<body class="antialiased">
+<div id="root"></div>
+</body>
 </html>
